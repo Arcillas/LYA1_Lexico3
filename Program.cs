aa74058b93722a2534c2c663f0718e7112113009
Arcillas <l21140773@queretaro.tecnm.mx>
2023-10-09 16:02:48 -0600

    Update ussing teacher's version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LYA1_Lexico3
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            try
+            {
+                using (Lexico L = new Lexico())
+                {
+                    while (!L.FinArchivo())
+                    {
+                        L.nextToken();
+                    }
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
