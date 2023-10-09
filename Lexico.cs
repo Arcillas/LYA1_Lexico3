9b64f7e149017b2673ff767cbd919197e811c1c4
Arcillas <l21140773@queretaro.tecnm.mx>
2023-10-09 17:45:52 -0600

    Add the TRAND matrix

--- Lexico.cs
+++ Lexico.cs
@@ -16,16 +16,42 @@ namespace LYA1_Lexico3
 
         int[,] TRAND =  
         {
-        //  WS,L,D,.,E,+,-,La
-            {0,1,2,8,1,8,8,8}, // 0
-            {F,1,1,F,1,F,F,F}, // 1
-            {F,F,2,3,5,F,F,F}, // 2
-            {E,E,4,E,E,E,E,E}, // 3
-            {F,F,4,F,5,F,F,F}, // 4
-            {E,E,7,E,E,6,6,E}, // 5
-            {E,E,7,E,E,E,E,E}, // 6
-            {F,F,7,F,F,F,F,F}, // 7
-            {F,F,F,F,F,F,F,F}, // 8
+        //  WS,L,D,.,E,=;,&,|,!,<,>,+,-,%,*,/,?,“,{,},EOF,EOL,LMD
+            {0,1,2,27,1,8,10,11,12,13,16,17,19,20,22,22,28,24,25,32,33,F,0,27},
+            {F,1,1,F,1,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,3,3,5,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {E,E,4,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E},
+            {F,F,4,F,5,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {E,E,7,E,E,F,F,F,F,F,F,F,6,6,F,F,F,F,F,F,F,F,F,E},
+            {E,E,7,E,E,F,F,F,F,F,F,F,E,E,F,F,F,F,F,F,F,F,F,E},
+            {F,F,7,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,9,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,14,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,14,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,15,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,18,F,F,F,F,F,18,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,18,F,F,F,F,F,18,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,21,F,F,F,F,F,F,21,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,21,F,F,F,F,F,F,F,21,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,23,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,26,25,25,E,25,25},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,23,F,F,F,F,F,F,F,F,F,30,29,F,F,F,F,F,F,F},
+            {29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,0,29},
+            {30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,31,30,30,30,30,30,E,30,30},
+            {30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,31,0,30,30,30,30,E,30,30},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+
         };
         public Lexico()
         {
@@ -62,7 +88,15 @@ namespace LYA1_Lexico3
                 return 6;
             else
                 return 7;
-            
+        }
+        private void clasificar(int estado)
+        {
+            switch (estado)
+            {
+                case 1: setClasificacion(Tipos.Identificador); break;
+                case 2: setClasificacion(Tipos.Numero); break;
+                case 8: setClasificacion(Tipos.Caracter); break;
+            }
         }
         public void nextToken()
         {
@@ -74,9 +108,10 @@ namespace LYA1_Lexico3
             while (estado >= 0)
             {
                 c = (char)archivo.Peek();
-                
-                estado = TRAND[estado,columna(c)];
 
+                estado = TRAND[estado,columna(c)];
+                clasificar(estado);
+                
                 if (estado >= 0)
                 {
                     if (estado > 0)
@@ -85,6 +120,10 @@ namespace LYA1_Lexico3
                     }
                     archivo.Read();
                 }
+            }
+            if (estado == E)
+            {
+                throw new Error("Lexico: Se espera un digito",log);
             }
             setContenido(buffer);
             log.WriteLine("[" + getContenido() + "] : " + getClasificacion());
