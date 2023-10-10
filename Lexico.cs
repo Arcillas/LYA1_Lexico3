39b152b09d62e50bf8d29f9c2f46a579810f8056
Arcillas <l21140773@queretaro.tecnm.mx>
2023-10-09 18:30:26 -0600

    Fix the Digit identifier on the TRAND matrix

--- Lexico.cs
+++ Lexico.cs
@@ -19,7 +19,7 @@ namespace LYA1_Lexico3
         //  WS,L,D,.,E,=;,&,|,!,<,>,+,-,%,*,/,?,“,{,},EOF,EOL,LMD
             {0,1,2,27,1,8,10,11,12,13,16,17,19,20,22,22,28,24,25,32,33,F,0,27},
             {F,1,1,F,1,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
-            {F,F,3,3,5,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
+            {F,F,2,3,5,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
             {E,E,4,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E},
             {F,F,4,F,5,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F},
             {E,E,7,E,E,F,F,F,F,F,F,F,6,6,F,F,F,F,F,F,F,F,F,E},
